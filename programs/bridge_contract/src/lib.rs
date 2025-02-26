82d75693e0714564f7d2ba1051c6a6179a892b60
MyronZhang <zhangzhishun1024@gmail.com>
2025-02-26 09:26:31 +0800

    add comment

--- programs/bridge_contract/src/lib.rs
+++ programs/bridge_contract/src/lib.rs
@@ -2,23 +2,29 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::spl_token::instruction::AuthorityType;
 use anchor_spl::token::{self, SetAuthority, Token, TokenAccount, Transfer};
 
+// Declaring the program ID for the bridge contract
 declare_id!("1TetRib49XZYuKBypgVao4JoTSKJYgtmnNCp4P132pp");
 
 #[program]
 pub mod bridge_contract {
     use super::*;
 
+    // Function to initialize the escrow account with an operator
     pub fn initialize(ctx: Context<Initialize>, operator: Pubkey) -> Result<()> {
         let escrow_account = &mut ctx.accounts.escrow_account;
+        // Setting the operator for the escrow account
         escrow_account.operator = operator;
         msg!("Initialized EscrowAccount with operator: {:?}", operator);
         Ok(())
     }
 
+    // Function to distribute SOL from the escrow account to a recipient
     pub fn distribute_sol(ctx: Context<DistributeSol>, amount: u64) -> Result<()> {
+        // Ensuring the amount is greater than zero
         require!(amount > 0, CustomError::InvalidAmount);
 
         let escrow_account = &ctx.accounts.escrow_account;
+        // Checking if the operator is authorized
         require_keys_eq!(
             escrow_account.operator,
             ctx.accounts.operator.key(),
@@ -28,16 +34,20 @@ pub mod bridge_contract {
         let recipient = &ctx.accounts.recipient;
         let mut adjusted_amount = amount;
         if recipient.lamports() == 0 {
+            // If the recipient has no lamports
+            // Adjusting the amount to cover the rent-exempt minimum balance
             adjusted_amount =
                 amount + Rent::get()?.minimum_balance(recipient.to_account_info().data_len());
         }
 
+        // Transferring the adjusted amount from the escrow account to the recipient
         **escrow_account.to_account_info().try_borrow_mut_lamports()? -= adjusted_amount;
         **recipient.to_account_info().try_borrow_mut_lamports()? += adjusted_amount;
 
         Ok(())
     }
 
+    // Function to authorize the operator for the sender's token account
     pub fn authorize_operator_once(ctx: Context<AuthorizeToken>) -> Result<()> {
         msg!("Authorize tokens...");
         msg!(
@@ -49,31 +59,31 @@ pub mod bridge_contract {
             &ctx.accounts.sender_token_account.key()
         );
         msg!(
-            "sender_authority Address: {}",
+            "Sender Authority Address: {}",
             &ctx.accounts.sender_token_account_authority.key()
         );
 
-        // Set the operator as the authority of the sender's token account
+        // Setting the operator as the authority of the sender's token account
         let cpi_accounts = SetAuthority {
-            account_or_mint: ctx.accounts.sender_token_account.to_account_info(),
+            account_or_mint: ctx.accounts.sender_token_account.to_account_info(), // The token account to set authority on
             current_authority: ctx
                 .accounts
                 .sender_token_account_authority
-                .to_account_info(), // Operator is the current authority
+                .to_account_info(), // Current authority of the token account
         };
-        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts); // Creating a CPI context
         token::set_authority(
             cpi_ctx,
             AuthorityType::AccountOwner,
-            Some(ctx.accounts.operator.key()),
+            Some(ctx.accounts.operator.key()), // New authority (operator)
         )?;
 
         Ok(())
     }
 
-    // Function to distribute SPL token to the recipient
+    // Function to distribute SPL tokens to the recipient
     pub fn distribute_token(ctx: Context<DistributeToken>, amount: u64) -> Result<()> {
-        msg!("Transferring tokens...");
+        msg!("Transferring tokens..."); // Logging the token transfer process
         msg!(
             "Mint: {}",
             &ctx.accounts.token_program.to_account_info().key()
@@ -84,13 +94,13 @@ pub mod bridge_contract {
         );
         msg!("To Token Address: {}", &ctx.accounts.recipient.key());
 
-        // Now transfer the tokens from the sender's account to the recipient
+        // Transferring the tokens from the sender's account to the recipient
         let cpi_accounts = Transfer {
-            from: ctx.accounts.sender_token_account.to_account_info(),
-            to: ctx.accounts.recipient.to_account_info(),
-            authority: ctx.accounts.operator.to_account_info(), // Operator performs the transfer
+            from: ctx.accounts.sender_token_account.to_account_info(), // Source token account
+            to: ctx.accounts.recipient.to_account_info(),              // Destination token account
+            authority: ctx.accounts.operator.to_account_info(), // Operator performing the transfer
         };
-        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts); // Creating a CPI context
         token::transfer(cpi_ctx, amount)?;
 
         Ok(())
@@ -100,10 +110,10 @@ pub mod bridge_contract {
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(init, payer = payer, space = 8 + 32)]
-    pub escrow_account: Account<'info, EscrowAccount>,
+    pub escrow_account: Account<'info, EscrowAccount>, // Escrow account to be initialized
     #[account(mut)]
-    pub payer: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub payer: Signer<'info>, // Signer who pays for the account creation
+    pub system_program: Program<'info, System>, // System program for account management
 }
 
 #[derive(Accounts)]
@@ -111,37 +121,37 @@ pub struct AuthorizeToken<'info> {
     #[account(mut, has_one = operator)]
     pub escrow_account: Account<'info, EscrowAccount>,
     #[account(mut)]
-    pub sender_token_account: AccountInfo<'info>,
-    pub sender_token_account_authority: Signer<'info>,
-    pub operator: AccountInfo<'info>,
-    pub token_program: Program<'info, Token>,
+    pub sender_token_account: AccountInfo<'info>, // Sender's token account
+    pub sender_token_account_authority: Signer<'info>, // Authority of the sender's token account
+    pub operator: AccountInfo<'info>,                  // Operator who is being authorized
+    pub token_program: Program<'info, Token>,          // Token program for token operations
 }
 
 #[derive(Accounts)]
 pub struct DistributeSol<'info> {
     #[account(mut, has_one = operator)]
     pub escrow_account: Account<'info, EscrowAccount>,
-    pub operator: Signer<'info>,
+    pub operator: Signer<'info>, // Operator performing the distribution
     #[account(mut)]
-    pub recipient: AccountInfo<'info>,
-    pub system_program: Program<'info, System>,
+    pub recipient: AccountInfo<'info>, // Recipient account for SOL
+    pub system_program: Program<'info, System>, // System program for account management
 }
 
 #[derive(Accounts)]
 pub struct DistributeToken<'info> {
     #[account(mut, has_one = operator)]
     pub escrow_account: Account<'info, EscrowAccount>,
-    pub operator: Signer<'info>,
+    pub operator: Signer<'info>, // Operator performing the distribution
     #[account(mut, owner = token::ID)]
-    pub sender_token_account: Account<'info, TokenAccount>,
+    pub sender_token_account: Account<'info, TokenAccount>, // Sender's token account
     #[account(mut, owner = token::ID)]
-    pub recipient: Account<'info, TokenAccount>,
-    pub token_program: Program<'info, Token>,
+    pub recipient: Account<'info, TokenAccount>, // Recipient token account
+    pub token_program: Program<'info, Token>, // Token program for token operations
 }
 
 #[account]
 pub struct EscrowAccount {
-    pub operator: Pubkey,
+    pub operator: Pubkey, // Public key of the operator managing the escrow account
 }
 
 #[error_code]
